3ccc8bda89292f8f51cf4db5391a1caf73bd622b
jmilanez <josiane.milanez@gmail.com>
2017-03-31 16:46:20 +0200

    Adding kinesis aggregation library

--- handler.py
+++ handler.py
@@ -2,8 +2,16 @@ import boto3
 import json
 import logging
 import base64
+import os
+import sys
+
 logger = logging.getLogger()
 logger.setLevel(logging.WARN)
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "./.requirements"))
+
+from aws_kinesis_agg.deaggregator import deaggregate_records
 
 client = boto3.client('s3')
 
@@ -15,7 +23,7 @@ def save(parsed_data, key, bucket_name):
     client.put_object(Bucket=bucket_name, Key=key, Body=parsed_data)
 
 def handle(event, context):
-    for record in event['Records']:
+    for record in deaggregate_records(event['Records']):
         payload = base64.b64decode(record['kinesis']['data'])
         try:
             parsed_data = json.loads(payload)
